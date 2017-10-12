9b0263e563d383debabb3cf8791e403a23e9da7b
KamilZet_ <kamilxzien@gmail.com>
2017-10-12 16:09:23 +0200

    Change to ViewBag

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Message = "Welcome to the PLaNet platform";
+            
             return View();
         }
 
